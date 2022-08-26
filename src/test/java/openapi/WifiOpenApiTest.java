1eb62c34f30001ab32af6e6fe4385362b239e3ed
ChoiYoungHoon <dudgns2865@naver.com>
2022-08-26 22:24:52 +0900

    test : open api 테스트 코드 작성

---
+++ src/test/java/openapi/WifiOpenApiTest.java
@@ -0,0 +1,37 @@
+package openapi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WifiOpenApiTest {
+
+    public static void main(String[] args) {
+        String key = "5555776a7264756437317a56427555";
+        try {
+            String apiUrl = "http://openapi.seoul.go.kr:8088/" + key + "/json/TbPublicWifiInfo/1/5/";
+            URL url = new URL(apiUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            int responseCode = con.getResponseCode();
+
+            BufferedReader br;
+            if (responseCode == 200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else { // 에러
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+        } catch (Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+    }
+
+}
