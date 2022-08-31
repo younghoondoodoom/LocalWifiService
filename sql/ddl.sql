8c757d62431015d1536bf60ecd284cd3101c6398
ChoiYoungHoon <dudgns2865@naver.com>
2022-09-01 00:32:04 +0900

    chore : ddl sql 저장

---
+++ sql/ddl.sql
@@ -0,0 +1,25 @@
+create table if not exists wifi
+(
+    id                     integer primary key autoincrement,
+    management_no          text,
+    borough                text,
+    road_name_address      text,
+    detail_address         text,
+    floor                  integer,
+    install_type           text,
+    install_institution    text,
+    service_classification text,
+    network_type           text,
+    installed_year         integer,
+    inout                  text,
+    connection_env         text,
+    lat                    real,
+    lnt                    real,
+    work_datetime          text
+);
+
+create table if not exists location_history (
+    id  integer primary key autoincrement,
+    lat real,
+    lnt real
+)
