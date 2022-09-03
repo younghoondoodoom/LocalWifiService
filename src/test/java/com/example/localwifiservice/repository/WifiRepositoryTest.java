a1479cbd32096a7e291d444cc0ae4fa7b288bdcd
ChoiYoungHoon <dudgns2865@naver.com>
2022-09-03 18:18:15 +0900

    test : wifi insert test code 작성

--- src/test/java/com/example/localwifiservice/repository/WifiRepositoryTest.java
+++ src/test/java/com/example/localwifiservice/repository/WifiRepositoryTest.java
@@ -26,4 +26,20 @@ class WifiRepositoryTest {
 
     }
 
+    @Test
+    public void insertWifiTest() throws Exception {
+        //given
+        wifiRepository = WifiRepository.getWifiRepository();
+        Wifi wifi = new Wifi("ARI00001", "서대문구", "상수도사업본부", "서소문로 51", "본관 1F", "1",
+            "7-1. 커뮤니티 - 행정", "서울시(AP)", "공공WiFi", "임대망", "2014", "실내", "",
+            126.96675, 37.561924, "2022-08-31 10:58:00.0");
+
+        //when
+        boolean b = wifiRepository.insertWifi(wifi);
+
+        //then
+        assertThat(b).isEqualTo(true);
+        assertThat(wifi.toString()).isEqualTo(wifiRepository.getWifiList().get(0).toString());
+    }
+
 }
