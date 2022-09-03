1f9f87c406da1eaf38625cd42b76191b205fb6c8
ChoiYoungHoon <dudgns2865@naver.com>
2022-09-03 17:34:46 +0900

    feat: serializer class에서 wifi 객체 리스트 반환하는 getWifiList 구현

--- src/main/java/com/example/localwifiservice/serializer/WifiSerializer.java
+++ src/main/java/com/example/localwifiservice/serializer/WifiSerializer.java
@@ -1,9 +1,10 @@
 package com.example.localwifiservice.serializer;
 
+import com.example.localwifiservice.entity.Wifi;
+import com.example.localwifiservice.serializer.WifiSerializer.TbPublicWifiInfo.Data;
+import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class WifiSerializer {
@@ -17,7 +18,6 @@ public class WifiSerializer {
         private Result RESULT;
         private List<Data> row;
 
-        @Getter
         public static class Result {
 
             String CODE;
@@ -25,7 +25,6 @@ public class WifiSerializer {
         }
 
 
-        @Getter
         public class Data {
 
             Long id;
@@ -49,5 +48,18 @@ public class WifiSerializer {
 
     }
 
+    public List<Wifi> getWifiList() {
+        ArrayList<Wifi> wifis = new ArrayList<>();
+
+        for (Data data : TbPublicWifiInfo.row) {
+            Wifi wifi = new Wifi(data.X_SWIFI_MGR_NO, data.X_SWIFI_WRDOFC, data.X_SWIFI_MAIN_NM,
+                data.X_SWIFI_ADRES1, data.X_SWIFI_ADRES2, data.X_SWIFI_INSTL_FLOOR,
+                data.X_SWIFI_INSTL_TY, data.X_SWIFI_INSTL_MBY, data.X_SWIFI_SVC_SE,
+                data.X_SWIFI_CMCWR, data.X_SWIFI_CNSTC_YEAR, data.X_SWIFI_INOUT_DOOR,
+                data.X_SWIFI_REMARS3, data.LAT, data.LNT, data.WORK_DTTM);
+            wifis.add(wifi);
+        }
+        return wifis;
+    }
 
 }
